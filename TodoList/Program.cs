56c3127118ac553d0feba7f323b838b22adf736c
Zaryamiya <julia.julia40006@gmail.com>
2025-09-27 01:34:57 +0300

    + Programm

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Task // Убрана точка с запятой после имени класса
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("the programm was made by Deinega and Piyagova");
+
+        Console.WriteLine("Tell me your name:");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("Tell me your surname:");
+        string sname = Console.ReadLine();
+
+        Console.WriteLine("Tell me your year of birth:");
+
+       
+        int birthYear = int.Parse(Console.ReadLine());
+        DateTime currentDate = DateTime.Today;
+        int age = currentDate.Year - birthYear;
+
+        
+        Console.WriteLine($"New user Added: {fname} {sname}, Age: {age}");
+    }
+}
