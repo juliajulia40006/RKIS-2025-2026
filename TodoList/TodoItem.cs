e530d66ae1a8d55bf2ccec701e18a71a7f399f28
Алеся Дейнега <alesadejnega@gmail.com>
2025-10-25 00:13:05 +0300

    lesson 6: added methods for TodoItems

--- TodoList/TodoItem.cs
+++ TodoList/TodoItem.cs
@@ -10,4 +10,44 @@
         IsDone = false;
         LastUpdate = DateTime.Now;
     }
+
+    public void MarkDone()
+    {
+        IsDone = true;
+        LastUpdate = DateTime.Now;
+    }
+
+    public void UpdateText(string newText)
+    {
+        Text = newText;
+        LastUpdate = DateTime.Now;
+    }
+
+    public string GetShortInfo()
+    {
+        string shortText = GetFirstLine(Text);
+        shortText = shortText.Length > 30 ? shortText.Substring(0, 27) + "..." : shortText;
+        string status = IsDone ? "выполнено" : "не выполнено";
+        return $"{shortText} | {status} | {LastUpdate:dd.MM.yyyy HH:mm}";
+    }
+
+    public string GetFullInfo()
+    {
+        string status = IsDone ? "выполнено" : "не выполнено";
+        return $"Текст: {Text}\nСтатус: {status}\nДата изменения: {LastUpdate}";
+    }
+
+    private string GetFirstLine(string task)
+    {
+        if (string.IsNullOrEmpty(task))
+            return task;
+
+        int newLineIndex = task.IndexOf('\n');
+        if (newLineIndex >= 0)
+        {
+            return task.Substring(0, newLineIndex) + "...";
+        }
+
+        return task;
+    }
 }
