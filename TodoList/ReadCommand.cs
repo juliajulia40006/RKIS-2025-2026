ecf378eec40dd9e0326d8c01d2954f41cd1d9157
Zaryamiya <julia.julia40006@gmail.com>
2025-10-31 20:51:29 +0300

    lesson 7: added ReadCommand

---
+++ TodoList/ReadCommand.cs
@@ -0,0 +1,20 @@
+﻿public class ReadCommand : ICommand
+{
+    public int TaskIndex { get; set; }
+    public TodoList TodoList { get; set; }
+
+    public void Execute()
+    {
+        if (TaskIndex >= 1 && TaskIndex <= TodoList.Count)
+        {
+            int index = TaskIndex - 1;
+            TodoItem item = TodoList.GetItem(index);
+            Console.WriteLine($"Задача #{TaskIndex}:");
+            Console.WriteLine(item.GetFullInfo());
+        }
+        else
+        {
+            Console.WriteLine("Неверный номер задачи!");
+        }
+    }
+}
