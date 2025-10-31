1d572fed8fc15d450d80b01e22a90c5dd821f5d1
Zaryamiya <julia.julia40006@gmail.com>
2025-10-31 20:46:02 +0300

    lesson 7: added DeleteCommand

---
+++ TodoList/DeleteCommand.cs
@@ -0,0 +1,22 @@
+﻿public class DeleteCommand : ICommand
+{
+    public int TaskIndex { get; set; }
+    public TodoList TodoList { get; set; }
+
+    public void Execute()
+    {
+        if (TaskIndex >= 1 && TaskIndex <= TodoList.Count)
+        {
+            int index = TaskIndex - 1;
+            TodoItem item = TodoList.GetItem(index);
+            string deletedTask = item.Text;
+
+            TodoList.Delete(index);
+            Console.WriteLine($"Задача '{deletedTask}' удалена.");
+        }
+        else
+        {
+            Console.WriteLine("Неверный номер задачи!");
+        }
+    }
+}
