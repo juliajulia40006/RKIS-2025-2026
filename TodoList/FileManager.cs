15bb5c03604c3dd223be15d211e6bef35d1cf420
Алеся Дейнега <alesadejnega@gmail.com>
2025-11-07 23:26:58 +0300

    lesson 8: added EnsureDataDirector method

--- TodoList/FileManager.cs
+++ TodoList/FileManager.cs
@@ -2,5 +2,11 @@
 
 public static class FileManager
 {
-
+    public static void EnsureDataDirectory(string dirPath)
+    {
+        if (!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+    }
 }
