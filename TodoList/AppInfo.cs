e71977ae30c245a984a6198b00a33488af444cc6
Zaryamiya <julia.julia40006@gmail.com>
2025-11-27 19:17:51 +0300

    lesson 10: added undoStack and redoStack

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -1,6 +1,10 @@
-﻿namespace TodoList;
-internal class AppInfo
+﻿using TodoList.Commands;
+
+namespace TodoList;
+class AppInfo
 {
 	public static TodoList Todos { get; set; }
 	public static Profile CurrentProfile { get; set; }
+	public static Stack<ICommand> undoStack = new Stack<ICommand>();
+	public static Stack<ICommand> redoStack = new Stack<ICommand>();
 }
