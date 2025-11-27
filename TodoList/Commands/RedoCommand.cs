b4062398b923319c74f47df4d2c8fc1cb56075f9
Zaryamiya <julia.julia40006@gmail.com>
2025-11-27 19:47:40 +0300

    lesson 10: created RedoCommand

---
+++ TodoList/Commands/RedoCommand.cs
@@ -0,0 +1,26 @@
+﻿using TodoList.Commands;
+
+namespace TodoList;
+
+public class RedoCommand : ICommand
+{
+	public void Execute()
+	{
+		if (AppInfo.redoStack.Count > 0)
+		{
+			ICommand command = AppInfo.redoStack.Pop();
+			command.Execute();
+			AppInfo.undoStack.Push(command);
+			Console.WriteLine("Команда повторена.");
+		}
+		else
+		{
+			Console.WriteLine("Нет команд для повтора.");
+		}
+	}
+
+	public void Unexecute()
+	{
+
+	}
+}
