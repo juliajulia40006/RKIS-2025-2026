e474b0c22aae94124347fb810c1f8e8a511415bd
Zaryamiya <julia.julia40006@gmail.com>
2025-12-06 02:47:06 +0300

    lesson 11: fixes appinfo modification

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -3,18 +3,37 @@
 namespace TodoList;
 class AppInfo
 {
-	public static TodoList Todos { get; set; }
 	public static List<Profile> Profiles { get; set; } = new List<Profile>();
 	public static Guid? CurrentProfileId { get; set; }
-	public static Dictionary<Guid, List<TodoItem>> UserTodos { get; set; } = new Dictionary<Guid, List<TodoItem>>();
-
+	public static Dictionary<Guid, TodoList> UserTodos { get; set; } = new Dictionary<Guid, TodoList>();
+	public static TodoList Todos { get; set; }
+	public static Profile CurrentProfile { get; set; }
 	public static Stack<ICommand> undoStack = new Stack<ICommand>();
 	public static Stack<ICommand> redoStack = new Stack<ICommand>();
-	public static List<TodoItem> GetCurrentTodos()
+	public static TodoList GetCurrentTodos()
 	{
 		if (CurrentProfileId.HasValue && UserTodos.ContainsKey(CurrentProfileId.Value))
+		{
 			return UserTodos[CurrentProfileId.Value];
-		return new List<TodoItem>();
+		}
+		return new TodoList();
+	}
+	public static void SaveUserTodos(Guid userId)
+	{
+		if (UserTodos.ContainsKey(userId))
+		{
+			string filePath = Path.Combine("data", $"todos_{userId}.csv");
+			FileManager.SaveTodos(UserTodos[userId], filePath);
+		}
+	}
+	public static TodoList LoadUserTodos(Guid userId)
+	{
+		string filePath = Path.Combine("data", $"todos_{userId}.csv");
+		if (File.Exists(filePath))
+		{
+			return FileManager.LoadTodos(filePath);
+		}
+		return new TodoList();
 	}
 
 }
