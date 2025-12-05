9d21fbf9760f4aa0a00de3bd75081179e6f50cc9
Zaryamiya <julia.julia40006@gmail.com>
2025-12-06 02:13:59 +0300

    lesson 11: update Profile.cs

--- TodoList/Profile.cs
+++ TodoList/Profile.cs
@@ -1,13 +1,19 @@
 ﻿namespace TodoList;
 public class Profile
 {
-    public string FirstName { get; private set; }
+	public Guid Id { get; private set; }
+	public string Login { get; private set; }
+	public string Password { get; private set; }
+	public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public int BirthYear { get; private set; }
 
-    public Profile(string firstName, string lastName, int birthYear)
+    public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
     {
-        FirstName = firstName;
+		Id = id;
+		Login = login;
+		Password = password;
+		FirstName = firstName;
         LastName = lastName;
         BirthYear = birthYear;
     }
