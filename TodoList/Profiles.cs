3e5ddaf179c75fb964208f8594c470cc67efd82a
Zaryamiya <julia.julia40006@gmail.com>
2025-12-06 02:19:38 +0300

    lesson 11: created profiles.cs

---
+++ TodoList/Profiles.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TodoList;
+public class Profiles
+{
+	private List<Profile> profiles = new List<Profile>();
+
+	public void Add(Profile profile)
+	{
+		profiles.Add(profile);
+	}
+
+	public Profile FindByLogin(string login)
+	{
+		return profiles.FirstOrDefault(p => p.Login.Equals(login, StringComparison.OrdinalIgnoreCase));
+	}
+
+	public Profile FindById(Guid id)
+	{
+		return profiles.FirstOrDefault(p => p.Id == id);
+	}
+
+	public IReadOnlyList<Profile> GetAll() => profiles.AsReadOnly();
+}
