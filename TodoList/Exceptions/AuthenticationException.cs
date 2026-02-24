cee0acd1b6d97ae88f4df8c75a856e5c48ce5d00
Zaryamiya <julia.julia40006@gmail.com>
2026-02-24 22:29:19 +0300

    lesson 14: added DuplicateLogin and Authentication exceptions

---
+++ TodoList/Exceptions/AuthenticationException.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Exceptions;
+
+public class AuthenticationException : Exception
+{
+	public AuthenticationException() : base("Ошибка аутентификации.") { }
+
+	public AuthenticationException(string message) : base(message) { }
+
+	public AuthenticationException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
---
+++ TodoList/Exceptions/DuplicateLoginException.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoList.Exceptions;
+
+public class DuplicateLoginException : Exception
+{
+	public string Login { get; }
+
+	public DuplicateLoginException() : base("Логин уже занят.") { }
+
+	public DuplicateLoginException(string login)
+		: base($"Логин '{login}' уже используется.")
+	{
+		Login = login;
+	}
+
+	public DuplicateLoginException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
