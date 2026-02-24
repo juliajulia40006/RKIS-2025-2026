2ecb212730441db7d63fbffd002eb871b43aa98b
Zaryamiya <julia.julia40006@gmail.com>
2026-02-25 00:41:21 +0300

    lesson 14: update UndoCommand

--- TodoList/Commands/UndoCommand.cs
+++ TodoList/Commands/UndoCommand.cs
@@ -1,27 +1,22 @@
-﻿
-namespace TodoList.Commands;
+﻿namespace TodoList.Commands;
 public class UndoCommand: ICommand
 {
 	public void Execute()
 	{
-		if (AppInfo.undoStack.Count > 0)
+		if (AppInfo.undoStack.Count == 0)
+			throw new InvalidOperationException("Нет команд для отмены.");
+
+		ICommand command = AppInfo.undoStack.Pop();
+		if (command is IUndo undoableCommand)
 		{
-			ICommand command = AppInfo.undoStack.Pop();
-			if (command is IUndo undoableCommand)
-			{
-				undoableCommand.Unexecute();
-				AppInfo.redoStack.Push(command);
-				Console.WriteLine("Команда отменена.");
-			}
-			else
-			{
-				AppInfo.undoStack.Push(command);
-				Console.WriteLine("Эта команда не поддерживает отмену.");
-			}
+			undoableCommand.Unexecute();
+			AppInfo.redoStack.Push(command);
+			Console.WriteLine("Команда отменена.");
 		}
 		else
 		{
-			Console.WriteLine("Нет команд для отмены.");
+			AppInfo.undoStack.Push(command);
+			throw new InvalidOperationException("Эта команда не поддерживает отмену.");
 		}
 	}
 
