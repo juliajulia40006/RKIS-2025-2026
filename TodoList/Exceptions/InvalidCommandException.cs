901281283a09c633aecb812223d8b8bd6c48ce24
Zaryamiya <julia.julia40006@gmail.com>
2026-02-24 22:17:19 +0300

    lesson 14: Addded InvalidCommandException

---
+++ TodoList/Exceptions/InvalidCommandException.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Exceptions;
+
+public class InvalidCommandException : Exception
+{
+	public InvalidCommandException() : base("Неизвестная команда.") { }
+
+	public InvalidCommandException(string message) : base(message) { }
+
+	public InvalidCommandException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
