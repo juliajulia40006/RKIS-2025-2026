a6e42b9d34db591cd7704caefef7013af40716a8
Zaryamiya <julia.julia40006@gmail.com>
2026-02-24 22:25:08 +0300

    lesson 14: added ProfileNotfoundException

---
+++ TodoList/Exceptions/ProfileNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoList.Exceptions;
+
+public class ProfileNotFoundException : Exception
+{
+	public string Login { get; }
+
+	public ProfileNotFoundException() : base("Профиль не найден.") { }
+	public ProfileNotFoundException(string login)
+		: base($"Профиль с логином '{login}' не найден.")
+	{
+		Login = login;
+	}
+
+	public ProfileNotFoundException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
