a80f58f53f34c194a7ee50993fb69f34da43add0
Zaryamiya <julia.julia40006@gmail.com>
2026-02-24 22:20:52 +0300

    lesson 14: added TaskNotFoundException

---
+++ TodoList/Exceptions/TaskNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoList.Exceptions;
+
+public class TaskNotFoundException : Exception
+{
+	public int TaskIndex { get; }
+
+	public TaskNotFoundException() : base("Задача не найдена.") { }
+
+	public TaskNotFoundException(string message) : base(message) { }
+
+	public TaskNotFoundException(int taskIndex)
+		: base($"Задача с индексом {taskIndex} не существует.")
+	{
+		TaskIndex = taskIndex;
+	}
+
+	public TaskNotFoundException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
