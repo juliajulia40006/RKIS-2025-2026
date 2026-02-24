1e3c2e7db87ccd24e39f8eb104d50472932f5b60
Zaryamiya <julia.julia40006@gmail.com>
2026-02-24 22:18:48 +0300

    lesson 14: added InvalidArgumentException.cs

---
+++ TodoList/Exceptions/InvalidArgumentException.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Exceptions;
+
+public class InvalidArgumentException : Exception
+{
+	public InvalidArgumentException() : base("Неверный аргумент команды.") { }
+
+	public InvalidArgumentException(string message) : base(message) { }
+
+	public InvalidArgumentException(string message, Exception innerException)
+		: base(message, innerException) { }
+}
