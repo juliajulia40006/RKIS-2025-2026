fec597d14ae676b65a3a35944aeb2aa351252ac7
Zaryamiya <julia.julia40006@gmail.com>
2026-02-28 14:36:24 +0300

    lesson 15: added load command

---
+++ TodoList/Commands/LoadCommand.cs
@@ -0,0 +1,64 @@
+﻿namespace TodoList.Commands;
+
+public class LoadCommand : ICommand
+{
+	public int DownloadsCount { get; set; }
+	public int DownloadSize { get; set; }
+
+	private static readonly object _consoleLock = new object();
+
+	public void Execute()
+	{
+		RunAsync().Wait();
+	}
+
+	private async Task RunAsync()
+	{
+		var tasks = new List<Task>();
+
+		for (int i = 0; i < DownloadsCount; i++)
+		{
+			int index = i;
+			tasks.Add(DownloadAsync(index));
+		}
+
+		await Task.WhenAll(tasks);
+
+		Console.WriteLine("\nВсе загрузки завершены.");
+	}
+
+	private async Task DownloadAsync(int index)
+	{
+		var random = new Random();
+
+		for (int i = 0; i <= DownloadSize; i++)
+		{
+			int percent = i * 100 / DownloadSize;
+			string bar = GenerateProgressBar(percent);
+
+			lock (_consoleLock)
+			{
+				Console.SetCursorPosition(0, Console.CursorTop - DownloadsCount + index);
+				Console.Write(bar);
+			}
+
+			await Task.Delay(random.Next(50, 200));
+		}
+	}
+
+	private string GenerateProgressBar(int percent)
+	{
+		int completedBars = percent / 5;
+		string bar = "[";
+
+		for (int i = 0; i < 20; i++)
+		{
+			bar += i < completedBars ? "#" : "-";
+		}
+
+		bar += $"] {percent}%";
+		return bar.PadRight(Console.WindowWidth - 1);
+	}
+
+	public void Unexecute() { }
+}
