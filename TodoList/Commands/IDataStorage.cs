786b4c47759996ecd9cd9f159f894bd818003041
Zaryamiya <julia.julia40006@gmail.com>
2026-02-28 17:23:53 +0300

    lesson 16: added IDataStorage.cs

---
+++ TodoList/Commands/IDataStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList;
+
+public interface IDataStorage
+{
+	void SaveProfiles(IEnumerable<Profile> profiles);
+	IEnumerable<Profile> LoadProfiles();
+	void SaveTodos(Guid userId, IEnumerable<TodoItem> todos);
+	IEnumerable<TodoItem> LoadTodos(Guid userId);
+}
