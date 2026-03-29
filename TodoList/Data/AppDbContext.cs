b5a4e640d2e1ca1ecee4c69941e616e5dc988c42
Zaryamiya <julia.julia40006@gmail.com>
2026-03-29 17:45:23 +0300

    lesson 19: added TodoRepository

---
+++ TodoList/Data/AppDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<TodoItem> Todos => Set<TodoItem>();
+	public DbSet<Profile> Profiles => Set<Profile>();
+
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite("Data Source=todos.db");
+	}
+
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<TodoItem>()
+			.HasOne(t => t.Profile)
+			.WithMany(p => p.Todos)
+			.HasForeignKey(t => t.ProfileId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		modelBuilder.Entity<Profile>()
+			.Property(p => p.Login)
+			.IsRequired()
+			.HasMaxLength(100);
+
+		modelBuilder.Entity<Profile>()
+			.Property(p => p.Password)
+			.IsRequired()
+			.HasMaxLength(100);
+
+		modelBuilder.Entity<Profile>()
+			.Property(p => p.FirstName)
+			.IsRequired()
+			.HasMaxLength(50);
+
+		modelBuilder.Entity<Profile>()
+			.Property(p => p.LastName)
+			.IsRequired()
+			.HasMaxLength(50);
+
+		modelBuilder.Entity<Profile>()
+			.Property(p => p.BirthYear)
+			.IsRequired();
+
+		modelBuilder.Entity<TodoItem>()
+			.Property(t => t.Text)
+			.IsRequired();
+
+		modelBuilder.Entity<TodoItem>()
+			.Property(t => t.Status)
+			.IsRequired();
+
+		modelBuilder.Entity<TodoItem>()
+			.Property(t => t.LastUpdate)
+			.IsRequired();
+	}
+}
---
+++ TodoList/Services/TodoRepository.cs
@@ -0,0 +1,52 @@
+﻿using TodoList.Models;
+using TodoList.Data;
+
+namespace TodoList.Services;
+
+public class TodoRepository
+{
+	public List<TodoItem> GetAll(Guid profileId)
+	{
+		using var context = new AppDbContext();
+		return context.Todos
+			.Where(t => t.ProfileId == profileId)
+			.ToList();
+	}
+
+	public void Add(TodoItem item, Guid profileId)
+	{
+		using var context = new AppDbContext();
+		item.ProfileId = profileId;
+		context.Todos.Add(item);
+		context.SaveChanges();
+	}
+
+	public void Update(TodoItem item)
+	{
+		using var context = new AppDbContext();
+		context.Todos.Update(item);
+		context.SaveChanges();
+	}
+
+	public void Delete(int id)
+	{
+		using var context = new AppDbContext();
+		var item = context.Todos.Find(id);
+		if (item != null)
+		{
+			context.Todos.Remove(item);
+			context.SaveChanges();
+		}
+	}
+	public void SetStatus(int id, TodoStatus status)
+	{
+		using var context = new AppDbContext();
+		var item = context.Todos.Find(id);
+		if (item != null)
+		{
+			item.SetStatus(status);
+			context.Todos.Update(item);
+			context.SaveChanges();
+		}
+	}
+}
