c6b5994dc7571f7a756c26251e6864ccfd274586
Zaryamiya <julia.julia40006@gmail.com>
2026-03-29 16:42:45 +0300

    lesson 19 (18): added edits in profile.cs

--- TodoList/Models/Profile.cs
+++ TodoList/Models/Profile.cs
@@ -1,14 +1,23 @@
-﻿namespace TodoList;
+﻿namespace TodoList.Models;
+using System.ComponentModel.DataAnnotations;
+
 public class Profile
 {
+	[Key]
 	public Guid Id { get; private set; }
+	[Required]
 	public string Login { get; private set; }
+	[Required]
 	public string Password { get; private set; }
+	[Required]
 	public string FirstName { get; private set; }
+	[Required]
     public string LastName { get; private set; }
     public int BirthYear { get; private set; }
 
-    public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
+	public ICollection<TodoItem> Todos { get; set; } = new List<TodoItem>();
+
+	public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
     {
 		Id = id;
 		Login = login;
@@ -19,7 +28,7 @@ public class Profile
     }
 
     public string GetInfo()
-    {
+    {	
         int age = DateTime.Today.Year - BirthYear;
         return $"{FirstName} {LastName}, возраст {age}";
     }
